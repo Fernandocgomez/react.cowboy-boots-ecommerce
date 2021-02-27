77a655d3dab603820fe47e4890f645d75cd381be
fernandocgomez <fernandocgomez@live.com>
2021-02-26 22:07:59 -0600

    Create adminLogin UI

--- src/components/Route-Components/AdminLogin/AdminLogin.js
+++ src/components/Route-Components/AdminLogin/AdminLogin.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import Footer from '../../Reusable-Components/Footer/Footer'
+import NavBar from '../../Reusable-Components/NavBar/NavBar'
+import TopNavBar from '../../Reusable-Components/TopNavBar/TopNavBar'
+import UserForm from '../../Reusable-Components/UserForm/UserForm'
 
 const AdminLogin = () => {
     return (
-        <div>
-            AdminLogin
+        <div className="admin-login">
+            <TopNavBar /> 
+            <NavBar />
+            <UserForm heading="User Login" type="adminLogin" />
+            <Footer />
         </div>
     )
 }
